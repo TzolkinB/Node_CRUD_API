5872f77e3a1114b3d91b382900c5b7addef5b5d8
Kimberly Bell <kimberly.bell@tma1.com>
2018-08-17 13:14:29 -0400

    added all CRUD routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,52 @@
+var ObjectID = require('mongodb').ObjectID; //mongoDB requires ID to be an object
+
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => { //post request to '/notes' path
-    console.log(req.body)
-    res.send('Create note')
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(item);
+      }
+    });
   });
+
+  app.post('/notes', (req, res) => { //post request to '/notes' path
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(note);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send('Note ' + id + ' was deleted');
+      }
+    });
+  });
+
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express     = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
+const db          = require('./config/db');
 
 const app = express(); //initialize the app as an instance of Express
 
@@ -8,7 +9,11 @@ const port = 8040;
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log(`Port ${port} is live`);
-});
+MongoClient.connect(db.url, (err, database) => { //use MongoClient to connect to db
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  
+  app.listen(port, () => {
+    console.log(`Port ${port} is live`);
+  });
+})
